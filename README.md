9847767b16d45475161388b3fd3e26d9a897536e
Untitled_dog <hansenrapp4@gmail.com>
2023-03-02 16:52:17 -0500

    foo

---
+++ README.md
@@ -0,0 +1,23 @@
+# foo
+*foo*
+
+**foo**
+
+***foo***
+
+~~foo~~
+
+```python
+print("foo")
+```
+
+foo:
+- foo
+- foo
+- foo
+
+
+foo
+1. foo
+2. foo
+3. foo
--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,7 @@ import java.io.IOException; // frick the IDE required because it said so
 import java.util.Scanner;  // Import the Scanner class
 import java.io.File;  // Import the File class
 import java.io.FileWriter;   // Import the FileWriter class
-import java.io.FileNotFoundException;  // Import this class to handle errors and pees in ur as
+import java.io.FileNotFoundException;  // Import this class to handle errors
 
 import static java.lang.System.*;   // So I don't have to type 'system' a lot
 
@@ -18,16 +18,16 @@ public class Main {
             x -= 1;//this actually works?
         }
         out.println("done");
-    }//pees in ur ass
-    public static void inputs(){//does the user know where piss goes?
-        Scanner scan = new Scanner(in);
-        out.println("where does pee go");
-        var peegoes = scan.nextLine();
-        if( peegoes.equals("ass") ){//yippee ka yay
-            out.println("indeed");
-        }else{ out.println("nah, in the [ass]"); }//tell the user to correct their behavior
     }
-    public static void remove_gay() {//using a function to remove the gay
+    public static void inputs(){//foo
+        Scanner scan = new Scanner(in);
+        out.println("foo");
+        var foos = scan.nextLine();
+        if( foos.equals("foo") ){//yippee ka yay
+            out.println("foo");
+        }else{ out.println("not foo"); }//tell the user to correct their behavior
+    }
+    public static void remove_file() {//using a function to remove the gay
         File myObj = new File("gay.txt");
         if (myObj.delete()) {
             out.println("removed the gay");
@@ -38,7 +38,7 @@ public class Main {
     public static void files(){
         Scanner scan = new Scanner(in);//using scanner as a sort of 'pause' function
         try {// try case
-            File myObj = new File("gay.txt");
+            File myObj = new File("foo.txt");
             if (myObj.createNewFile()) {
                 out.println("File created: " + myObj.getName());
             } else {
@@ -48,26 +48,27 @@ public class Main {
             out.println("An error occurred.");
             e.printStackTrace();
         }
-        out.println("[gay.txt] creation process done");
+        out.println("[foo.txt] creation process done");
         try {
-            FileWriter myWriter = new FileWriter("gay.txt");
-            out.println("type something in to write to the gay");
+            FileWriter myWriter = new FileWriter("foo.txt");
+            out.println("type something in to write to the foo.txt");
             String userinput = scan.nextLine();
             myWriter.write(userinput);
             myWriter.close();
-            out.println("check gay.txt");
+            out.println("check foo.txt");
         } catch (IOException e) {
             out.println("An error occurred.");
             e.printStackTrace();
         }
-        out.println("press enter to read the gay");
+        out.println("press enter to read the foo");
         scan.nextLine();
         /* RATIO; was lazy so didn't implement the read thing
         * instead just return user input
         * System.out.println(userinput); // nevermind java doesn't allow vars from things in try & catch
-        * actually have to do it */
-        try {//actually read from the gay because java doesn't let me be a lazy ass
-            File myObj = new File("gay.txt");
+        * actually have to do it
+        * edit: looking back i could have use global */
+        try {//actually read from the foo because java doesn't let me be a lazy ass
+            File myObj = new File("foo.txt");
             Scanner myReader = new Scanner(myObj);
             while (myReader.hasNextLine()) {
                 String data = myReader.nextLine();
@@ -78,9 +79,9 @@ public class Main {
             System.out.println("An error occurred.");
             e.printStackTrace();
         }
-        out.println("press enter to remove the gay");
+        out.println("press enter to remove the foo");
         scan.nextLine();
-        remove_gay();//use a method to remove the gay
+        remove_foo();//use a method to remove the foo
         System.out.println("files done");
 
     }
